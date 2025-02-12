8ae17fe9a69d98e2f543a3fdab964bffbecb7f60
Seth <seth.bassett001@albright.edu>
2025-02-12 13:11:39 -0500

    Delete CSC_142 directory

--- CSC_142/hello_world_two.cpp
+++
@@ -1,9 +0,0 @@
-# include <iostream> // Required for input and output
-
-int main() {
-    // Print "Hello, world!" to the console
-    std::cout << "Hello, World!" << std::endl;
-
-    // Return 0 to indicate successful execution, optional but good backwards compatible
-    return 0;
-}
