6aa05f815ab30fb476d47b9e605d2efaeea02bf7
Seth <seth.bassett001@albright.edu>
2025-03-27 21:39:41 -0400

    Add files via upload

---
+++ Week 10/easy.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// Uses a reference to increase hero's HP
+
+void drinkPotion(int& hp, int healing) {
+    hp += healing;
+
+hp = hp + healing;
+}
+
+// Uses a pointer to reduce hero's HP
+
+void damageHero(int* hp, int damage) {
+    *hp -= damage;
+}
+
+int main() {
+    int heroHP = 20;
+
+    std::cout << "Hero's starting HP: " << heroHP << std::endl;
+
+    std::cout << "Hero drinks a healing potion (+5 HP)." << std::endl;
+    drinkPotion(heroHP, 5);
+    std::cout << "Hero's current HP: " << heroHP << std::endl;
+
+    std::cout << "Hero takes damage from a goblin (-12 HP)." << std::endl;
+    damageHero(&heroHP, 12);
+    std::cout << "Hero's current HP: " << heroHP << std::endl;
+
+    return 0;
+}
